b6438691931bd2bf9b3c91cd45e738c3592a0296
Jerry <eghnay@gmail.com>
2022-07-26 22:08:55 +0300

    First Commit

---
+++ app/src/main/java/com/example/degiskenlerveveritipleri/DegiskenOlusturma.kt
@@ -0,0 +1,29 @@
+package com.example.degiskenlerveveritipleri
+
+fun main(){
+
+    var ogrenciAdi = "bilge"
+    var ogrenciYas = 24
+    var ogrenciBoy = 1.63f
+    var ogrenciBasHarf = "b"
+
+    println(ogrenciAdi)
+    println(ogrenciYas)
+    println(ogrenciBoy)
+    println(ogrenciBasHarf)
+
+
+    var urun_ıd:Int = 3416
+    var urun_ad:String = "Kol Saati"
+    var urun_adet:Int = 100
+    var urun_fiyat:Double = 109.99
+    var urun_tedarikci:String = "rolex"
+
+    println(urun_ıd)
+    println(urun_ad)
+    println(urun_adet)
+    println(urun_fiyat)
+    println(urun_tedarikci)
+
+    println("$urun_tedarikci marka $urun_ad $urun_fiyat fiyatla satılmıştır. Stokta $urun_adet kalmıştır")
+}
